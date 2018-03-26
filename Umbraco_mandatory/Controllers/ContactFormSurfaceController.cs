6a17bbbd41b429b268100e2efdef6aafc9a20ebe
Someone <Patrickholsths@gmail.com>
2018-03-26 20:14:55 +0200

    Umbraco-Init

---
+++ Umbraco_mandatory/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco_mandatory.ViewModels;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco_mandatory.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+    }
+}
---
+++ Umbraco_mandatory/ViewModels/ContactForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Umbraco_mandatory.ViewModels
+{
+    public class ContactForm
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
